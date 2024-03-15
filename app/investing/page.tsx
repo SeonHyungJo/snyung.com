781739b34a7d3255e56f8189862b1f7abf99dd40
SeonHyungJo <seonhyung.jo@gmail.com>
2024-03-16 01:16:36 +0900

    FEAT : about me 페이지 업데이트

--- app/investing/page.tsx
+++ app/investing/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { getAllInvestingPosts } from "../_api/getAllInvestingPosts";
 import PostListItem from "../_components/PostListItem";
 import TopSection from "../_components/TopSection";
@@ -10,6 +12,13 @@ export default async function SnippetsMainPage() {
       <TopSection
         title={`Investing(${posts.length})`}
         content={"⚡️ 유용한 자산 정보를 모아놓은 공간입니다."}
+        footer={
+          <section className={"py-2 px-2"}>
+            <Link href={"/snyung"} className={"text-slate-400"}>
+              {"More about me >"}
+            </Link>
+          </section>
+        }
       />
 
       <section className="flex flex-col items-start justify-start gap-2">
