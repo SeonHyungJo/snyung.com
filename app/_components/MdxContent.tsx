b7ba0241261e2a266ffc7407c32e2a2cb9d97ace
SeonHyungJo <seonhyung.jo@gmail.com>
2024-03-19 22:02:15 +0900

    FIX : Table scroll 처리

--- app/_components/MdxContent.tsx
+++ app/_components/MdxContent.tsx
@@ -15,12 +15,19 @@ const MdxComponents = {
       blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8MBMAAj8Ba+8o2i0AAAAASUVORK5CYII="}
       alt={"Posts Image"}
       {...props} />
+  ),
+  table: ({children, ...props}: any) =>  (
+    <div className="overflow-x-scroll w-auto">
+      <table {...props}>
+        {children}
+      </table>
+    </div>
   )
 };
 
 export function MdxContent({ serialized }: Pick<Post<Frontmatter>, "serialized">) {
   return (
-    <div className="prose max-w-none prose-code:before:content-[''] prose-code:after:content-[''] prose-code:rounded-lg prose-code:px-1.5 prose-code:py-1 prose-code:bg-slate-200 dark:prose-dark prose-img:max-w-none prose-img:rounded-xl prose-a:text-stale-300 
+    <article className="prose max-w-700 prose-code:before:content-[''] prose-code:after:content-[''] prose-code:rounded-lg prose-code:px-1.5 prose-code:py-1 prose-code:bg-slate-200 dark:prose-dark prose-img:max-w-none prose-img:rounded-xl prose-a:text-stale-300 
     lg:prose-h1:text-4xl prose-h1:text-3xl
     lg:prose-h2:text-3xl prose-h2:text-2xl
     lg:prose-h3:text-2xl prose-h3:text-xl
@@ -29,6 +36,6 @@ export function MdxContent({ serialized }: Pick<Post<Frontmatter>, "serialized">
     lg:prose-h6:text-base prose-h6:text-base
     ">
       <MDXRemote {...serialized} components={MdxComponents} />
-    </div>
+    </article>
   );
 }
