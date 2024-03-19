890fb9eee50d7b5ba061d5050932c354b658f481
SeonHyungJo <seonhyung.jo@gmail.com>
2024-03-19 21:53:28 +0900

    FIX : 모바일 기준 폰트 크기 수정

--- app/_components/MdxContent.tsx
+++ app/_components/MdxContent.tsx
@@ -9,7 +9,7 @@ const MdxComponents = {
   img: (props: any) => (
     <Image
       className={"w-full max-w-2xl"}
-      width={680}
+      width={700}
       height={320}
       placeholder={"blur"}
       blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8MBMAAj8Ba+8o2i0AAAAASUVORK5CYII="}
@@ -20,7 +20,14 @@ const MdxComponents = {
 
 export function MdxContent({ serialized }: Pick<Post<Frontmatter>, "serialized">) {
   return (
-    <div className="prose max-w-none prose-code:before:content-[''] prose-code:after:content-[''] prose-code:rounded-lg prose-code:px-1.5 prose-code:py-1 prose-code:bg-slate-200 dark:prose-dark prose-img:max-w-none prose-img:rounded-xl prose-a:text-stale-300">
+    <div className="prose max-w-none prose-code:before:content-[''] prose-code:after:content-[''] prose-code:rounded-lg prose-code:px-1.5 prose-code:py-1 prose-code:bg-slate-200 dark:prose-dark prose-img:max-w-none prose-img:rounded-xl prose-a:text-stale-300 
+    lg:prose-h1:text-4xl prose-h1:text-3xl
+    lg:prose-h2:text-3xl prose-h2:text-2xl
+    lg:prose-h3:text-2xl prose-h3:text-xl
+    lg:prose-h4:text-xl prose-h4:text-lg
+    lg:prose-h5:text-lg prose-h5:text-base
+    lg:prose-h6:text-base prose-h6:text-base
+    ">
       <MDXRemote {...serialized} components={MdxComponents} />
     </div>
   );
